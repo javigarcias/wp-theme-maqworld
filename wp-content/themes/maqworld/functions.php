db044004ae21bcce42fe4bc2a2f721682f4378b3
javigarcias <javigarciaweb@gmail.com>
2021-11-16 17:45:58 +0100

    Habilito modulo Imagen Destacada en las paginas

--- wp-content/themes/maqworld/functions.php
+++ wp-content/themes/maqworld/functions.php
@@ -1,5 +1,13 @@
 <?php
 
+// Cuando el tema esta activado
+function maqworld_setup() {
+
+    //Habilita imagenes destacadas
+    add_theme_support('post-thumbnails');
+}
+add_action('after_setup_theme', 'maqworld_setup');
+
 //Menus de navegacion
 function maqworld_menus() {
     register_nav_menus(array(
--- wp-content/themes/maqworld/page.php
+++ wp-content/themes/maqworld/page.php
@@ -3,6 +3,13 @@
 
 <?php while (have_posts()) : the_post(); ?>
     <h1><?php the_title(); ?></h1>
+
+    <?php 
+        if(has_post_thumbnail()):
+            the_post_thumbnail();
+        endif;
+    ?>
+
     <?php the_content(); ?>
 <?php endwhile; ?>
 
