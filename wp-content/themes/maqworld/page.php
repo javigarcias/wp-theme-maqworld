ac1ca82f0157dc952f2c7aef08b80e24f65455ca
javigarcias <javigarciaweb@gmail.com>
2021-11-17 11:33:50 +0100

    Abstraigo codigo repetido del template Pages en template/parts

--- wp-content/themes/maqworld/page.php
+++ wp-content/themes/maqworld/page.php
@@ -1,22 +1,13 @@
 <?php get_header(); ?>
 
-    <main class="contenedor pagina seccion con-sidebar">
-        <div class="contenido-principal">
-            <?php while (have_posts()) : the_post(); ?>
-                <h1 class="text-center texto-primario"><?php the_title(); ?></h1>
+<main class="contenedor pagina seccion con-sidebar">
 
-                <?php
-                if (has_post_thumbnail()) :
-                    the_post_thumbnail('blog', array('class' => 'imagen-destacada'));
-                endif;
-                ?>
-
-                <?php the_content(); ?>
-            <?php endwhile; ?>
-        </div>
-
-        <?php get_sidebar(); ?>
-    </main>
+    <div class="contenido-principal">
+        <?php get_template_part('template-parts/paginas'); ?>
+    </div>
+    
+    <?php get_sidebar(); ?>
+</main>
 
 
 <?php get_footer(); ?>
