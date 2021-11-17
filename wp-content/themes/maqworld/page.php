a5bb06876e7622ad883b8e129f61145c7074feed
javigarcias <javigarciaweb@gmail.com>
2021-11-17 10:25:27 +0100

    Implementando estilos en el template Page.php

--- wp-content/themes/maqworld/page.php
+++ wp-content/themes/maqworld/page.php
@@ -1,16 +1,22 @@
 <?php get_header(); ?>
 
+    <main class="contenedor pagina seccion con-sidebar">
+        <div class="contenido-principal">
+            <?php while (have_posts()) : the_post(); ?>
+                <h1 class="text-center texto-primario"><?php the_title(); ?></h1>
 
-<?php while (have_posts()) : the_post(); ?>
-    <h1><?php the_title(); ?></h1>
+                <?php
+                if (has_post_thumbnail()) :
+                    the_post_thumbnail('blog', array('class' => 'imagen-destacada'));
+                endif;
+                ?>
 
-    <?php 
-        if(has_post_thumbnail()):
-            the_post_thumbnail();
-        endif;
-    ?>
+                <?php the_content(); ?>
+            <?php endwhile; ?>
+        </div>
 
-    <?php the_content(); ?>
-<?php endwhile; ?>
+        <?php get_sidebar(); ?>
+    </main>
+
 
 <?php get_footer(); ?>
