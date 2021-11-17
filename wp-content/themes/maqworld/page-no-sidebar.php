6f3576f9d2462d1757d3fae777321a07e1137360
javigarcias <javigarciaweb@gmail.com>
2021-11-17 10:37:53 +0100

    Implemento nuevo template tipo pagina sin sidebar

---
+++ wp-content/themes/maqworld/page-no-sidebar.php
@@ -0,0 +1,25 @@
+<?php 
+/*
+* Template Name: Contenido Centrado SIN Sidebar
+*/
+
+get_header(); ?>
+
+    <main class="contenedor pagina seccion no-sidebar">
+        <div class="contenido-principal">
+            <?php while (have_posts()) : the_post(); ?>
+                <h1 class="text-center texto-primario"><?php the_title(); ?></h1>
+
+                <?php
+                if (has_post_thumbnail()) :
+                    the_post_thumbnail('blog', array('class' => 'imagen-destacada'));
+                endif;
+                ?>
+
+                <?php the_content(); ?>
+            <?php endwhile; ?>
+        </div>
+    </main>
+
+
+<?php get_footer(); ?>
