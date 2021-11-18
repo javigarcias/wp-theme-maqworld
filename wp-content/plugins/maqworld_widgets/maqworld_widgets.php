c4def93f9e0074e6cd8409f2f0559e5cf198c8e6
javigarcias <javigarciaweb@gmail.com>
2021-11-18 15:55:42 +0100

    Implemento Widget personalizado de plataformas

---
+++ wp-content/plugins/maqworld_widgets/maqworld_widgets.php
@@ -0,0 +1,121 @@
+<?php
+/*
+    Plugin Name: MaqWorld - Widgets
+    Plugin URI:
+    Description: Añade Widgets Personalizados en la web
+    Version: 1.0.0
+    Author: Javier García Sánchez
+    Author URI: https://www.linkedin.com/in/javigarciasanchez/
+    Text Domain: maqworld
+*/
+if(!defined('ABSPATH')) die(); //Bloquea la entrada si alguien quiere ver el contenido del plugin insertando URL
+
+/**
+ * Adds Maqworld_Plataformas_Widget widget.
+ */
+class Maqworld_Plataformas_Widget extends WP_Widget {
+
+	/**
+	 * Register widget with WordPress.
+	 */
+	function __construct() {
+		parent::__construct(
+			'Maqworld_Plataformas_Widget', // Base ID
+			esc_html__( 'Maqworld Plataformas', 'text_domain' ), // Name
+			array( 'description' => esc_html__( 'Agrega las plataformas como widget', 'text_domain' ), ) // Args
+		);
+	}
+
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @see WP_Widget::widget()
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
+	 */
+	public function widget( $args, $instance ) {
+		echo $args['before_widget'];
+		if ( ! empty( $instance['title'] ) ) {
+			echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ) . $args['after_title'];
+		}
+        ?>
+        <ul>
+            <?php
+                $args = array(
+                    'post_type' => 'maqworld_plataformas',
+                    'post_per_page' => 3,
+                );
+                $plataformas = new WP_Query($args);
+                while($plataformas->have_posts()): $plataformas->the_post();
+
+            ?>
+
+            <li class="plataforma-sidebar">
+                <div class="imagen">
+                    <?php the_post_thumbnail('thumbnail'); ?>
+                </div>
+
+                <div class="contenido-plataforma">
+                    <a href="<?php the_permalink(); ?>">
+                        <h3><?php the_title() ?></h3>
+                    </a>
+                    <?php
+                        $marca = get_field('marca');
+                        $precio = get_field('precio'); 
+                    ?>
+                    <p class="info-plataforma"> <?php echo $marca . " - " . $precio ?> </p>
+                </div>
+            </li>
+
+
+            <?php endwhile; wp_reset_postdata(); ?>
+        </ul>
+
+
+
+        <?php
+		echo $args['after_widget'];
+	}
+
+	/**
+	 * Back-end widget form.
+	 *
+	 * @see WP_Widget::form()
+	 *
+	 * @param array $instance Previously saved values from database.
+	 */
+	public function form( $instance ) {
+		$title = ! empty( $instance['title'] ) ? $instance['title'] : esc_html__( 'New title', 'text_domain' );
+		?>
+		<p>
+		<label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php esc_attr_e( 'Title:', 'text_domain' ); ?></label> 
+		<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+		</p>
+		<?php 
+	}
+
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 *
+	 * @return array Updated safe values to be saved.
+	 */
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? sanitize_text_field( $new_instance['title'] ) : '';
+
+		return $instance;
+	}
+
+} // class Maqworld_Plataformas_Widget
+
+// register Maqworld_Plataformas_Widget widget
+function register_Maqworld_Plataformas_Widget() {
+    register_widget( 'Maqworld_Plataformas_Widget' );
+}
+add_action( 'widgets_init', 'register_Maqworld_Plataformas_Widget' );
